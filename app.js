6046aa1df239b12c5dcf47caa6b529ed8ac04183
twumm <martint.mensah@gmail.com>
2018-06-19 16:08:25 +0000

    feat: Add mlabs mongodb url.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const MongoStore = require('connect-mongo')(session);
 
 // Set up mongoose connection.
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://127.0.0.1:27017/itemcatalog';
+const mongoDB = 'mongodb://twumm:itemcatalog18@ds263520.mlab.com:63520/itemcatalog';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -18,15 +18,15 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Use sessions for tracking logins
 app.use(session({
-    secret: 'awesome',
-    resave: true,
-    saveUninitialized: false,
-    // cookie: {
-    //     maxAge: 600000
-    // },
-    store: new MongoStore({
-        mongooseConnection: db
-    })
+  secret: 'awesome',
+  resave: true,
+  saveUninitialized: false,
+  // cookie: {
+  //     maxAge: 600000
+  // },
+  store: new MongoStore({
+    mongooseConnection: db
+  })
 }));
 
 const index = require('./routes/index');
@@ -52,20 +52,20 @@ app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
